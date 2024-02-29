d055c06b7f70b015e39bebb9cf6eb5138821a5eb
nosignal91 <161769810+nosignal91@users.noreply.github.com>
2024-02-29 23:06:26 +0200

    this code make you know IP target for any DNS But I am not responsible for any Electronically  gram
    hello i am nosignal91:)

---
+++ net-ip.py
@@ -0,0 +1,8 @@
+print("if you have a any proplem with the script check the wifi and if the this webside is real")
+import socket
+host = input(str("enter your dns:"))
+ip = socket.gethostbyname(host)
+os.system("clear")
+print(ip)
+os.system("bash")
+#this code will be know any url dns!!
